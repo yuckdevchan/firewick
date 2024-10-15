a648da2e20c311f730dc98fa09f2ff5465c9e1b9
Ethan Martin <60288171+yuckdevchan@users.noreply.github.com>
2024-10-16 00:15:18 +0100

    Audio support, mobile support, plyer integration, more

--- iv_api.py
+++ iv_api.py
@@ -1,8 +1,12 @@
-import requests
+import requests, humanize
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
+
+def get_instances() -> dict:
+    instances = requests.get("https://api.invidious.io/instances.json?pretty=1&sort_by=type,users")
+    return instances.json()
 
 def get_stats(invidious_instance: str) -> dict:
     stats = requests.get(f"{invidious_instance}/api/v1/stats")
@@ -10,8 +14,12 @@ def get_stats(invidious_instance: str) -> dict:
 
 def get_videos(invidious_instance: str, feed: str) -> dict:
     videos = requests.get(f"{invidious_instance}/api/v1/{feed}").json()
+    for video in videos:
+        video["viewCountCommas"] = humanize.intcomma(video["viewCount"])
+        video["viewCountText"] = humanize.intword(video["viewCount"]).replace(" thousand", "K").replace(" million", "M").replace(" billion", "B").replace(" trillion", "T")
     return videos
 
 def get_video(invidious_instance: str, video_id: str) -> dict:
     video = requests.get(f"{invidious_instance}/api/v1/videos/{video_id}", headers=headers).json()
+    video["viewCountCommas"] = humanize.intcomma(video["viewCount"])
     return video
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, send_from_directory
 import iv_api
 
 app = Flask(__name__)
 app.config['TESTING'] = True
+app.secret_key = b'\x83\xc7\x91\xb7\xfc\x16\t\xc1\xc2\xad\xc6\xf1'
 
 branding = {
     "name": "firewick"
@@ -10,10 +11,18 @@ branding = {
 
 iv_instance = "https://inv.nadeko.net"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", branding_name=branding["name"]), 404
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory("static", "favicon.ico", mimetype="image/vnd.microsoft.icon")
+
 @app.route("/")
 def index():
     popular_videos = iv_api.get_videos(iv_instance, "popular")
-    return render_template("videos.html", branding_name=branding["name"], videos=popular_videos)
+    return render_template("videos.html", branding_name=branding["name"], videos=popular_videos, accurate_view_counts=session.get("accurate_view_counts"))
 
 @app.route("/popular")
 def popular():
@@ -22,24 +31,35 @@ def popular():
 @app.route("/trending")
 def trending():
     trending_videos = iv_api.get_videos(iv_instance, "trending")
-    return render_template("videos.html", branding_name=branding["name"], videos=trending_videos)
+    return render_template("videos.html", branding_name=branding["name"], videos=trending_videos, accurate_view_counts=session.get("accurate_view_counts"))
 
 @app.route("/search")
 def search():
     query = request.args.get("q")
     search_results = iv_api.get_videos(iv_instance, f"search?q={query}")
-    return render_template("videos.html", branding_name=branding["name"], videos=search_results, query=query)
+    return render_template("videos.html", branding_name=branding["name"], videos=search_results, query=query, accurate_view_counts=session.get("accurate_view_counts"))
 
 @app.route("/watch")
 def watch():
     video_id = request.args.get("v")
     video = iv_api.get_video(iv_instance, video_id)
-    return render_template("watch.html", branding_name=branding["name"], video=video)
+    return render_template("watch.html", branding_name=branding["name"], video=video, accurate_view_counts=session.get("accurate_view_counts"))
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html", branding_name=branding["name"], settings=settings, instances=iv_api.get_instances(), accurate_view_counts=session.get("accurate_view_counts"))
 
 @app.route("/stats")
 def stats():
     stats = iv_api.get_stats(iv_instance)
     return render_template("stats.html", branding_name=branding["name"], stats=stats)
 
+@app.route("/api/settings/accurateViewCounts", methods=["POST"])
+def api_accurate_view_counts():
+    value = True if request.form.get("Accurate View Counts") == "on" else False
+    print(value)
+    session["accurate_view_counts"] = value
+    return "true"
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
